b051026305f739d563a0448c34f19cca1994f821
sahyunjin <tkguswls1106@naver.com>
2024-08-17 04:29:07 +0900

    Chore: 체킹 토큰로직 주석 제거

--- src/utils/CheckToken.js
+++ src/utils/CheckToken.js
@@ -7,24 +7,6 @@ function CheckToken() {
     if(!storedAccessToken || !storedRefreshToken) {
         window.location.href = "/login";
     }
-
-    // const storedToken = localStorage.getItem("accessToken");
-    // const storedExpirationDate = localStorage.getItem('expirationTime') || '0';
-
-    // if (storedToken) {
-    //     axios.defaults.headers.common['Authorization'] = `Bearer ${storedToken}`;
-
-    //     const remainingTime = storedExpirationDate - String(new Date().getTime());
-    //     if (remainingTime <= '1000') {
-    //         localStorage.removeItem('accessToken');
-    //         localStorage.removeItem('expirationTime');
-
-    //         window.location.href = '/login';
-    //     }
-    // }
-    // else {
-    //     window.location.href = '/login';
-    // }
 }
 
 export { CheckToken };
