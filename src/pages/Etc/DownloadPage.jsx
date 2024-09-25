ee58c565c6ca4fc992398190987ada74b11972d3
sahyunjin <tkguswls1106@naver.com>
2024-09-25 16:51:24 +0900

    Fix: 앱 다운로드 공지 수정

--- src/pages/Etc/DownloadPage.jsx
+++ src/pages/Etc/DownloadPage.jsx
@@ -82,7 +82,7 @@ const DivWrapper = styled.div`
         }
     }
 
-    #notCenter {
+    .notCenter {
         text-align: left;
 
         ul {
@@ -100,19 +100,24 @@ const Download = () => {
             <div style={{ lineHeight: "185%" }}><br></br></div>
             <h5>&lt;&nbsp;Android&nbsp;&nbsp;<i className="fa fa-android" aria-hidden="true"></i>&nbsp;&gt;</h5>
             <div style={{ lineHeight: "35%" }}><br></br></div>
-            Play Store 다운로드:&nbsp;&nbsp;<a href="https://play.google.com/store/apps/details?id=com.shj.onlinememo"><i className="fa fa-download" aria-hidden="true"></i></a>
+            <div className="notCenter">
+                <ul>
+                    <li>방법 1: Play Store 다운로드:&nbsp;&nbsp;<a href="https://play.google.com/store/apps/details?id=com.shj.onlinememo"><i className="fa fa-download" aria-hidden="true"></i></a></li>
+                    <li>방법 2: iOS와 동일한 방법으로 설치</li>
+                </ul>
+            </div>
             <div style={{ lineHeight: "150%" }}><br></br></div>
 
             <h5>&lt;&nbsp;iOS&nbsp;&nbsp;<i className="fa fa-apple" aria-hidden="true"></i>&nbsp;&gt;</h5>
             <div style={{ lineHeight: "35%" }}><br></br></div>
-            iOS 기기는 별도의 앱 다운로드 없이<br></br>
-            아래의 방법대로 앱처럼 이용 가능합니다.
+            별도의 앱 다운로드 없이<br></br>
+            밑의 과정으로 앱 설치가 가능합니다.
             <div style={{ lineHeight: "20%" }}><br></br></div>
-            <div id="notCenter">
+            <div className="notCenter">
                 <ul>
-                    <li>과정 1: Safari 앱 접속</li>
-                    <li>과정 2: 'www.OnlineMemo.kr' 주소 이동</li>
-                    <li>과정 3: 옵션에서 '홈 화면에 추가' 클릭</li>
+                    <li>과정 1: Chrome 또는 Safari 앱 접속</li>
+                    <li>과정 2: 'www.OnlineMemo.kr' 이동</li>
+                    <li>과정 3: 옵션의 '홈 화면에 추가' 클릭</li>
                 </ul>
             </div>
             <div style={{ lineHeight: "165%" }}><br></br></div>
@@ -120,7 +125,7 @@ const Download = () => {
             <strong>
                 <i className="fa fa-arrow-down" aria-hidden="true"></i>
                 <div style={{ lineHeight: "70%" }}><br></br></div>
-                모바일에서도 풀스크린으로 쾌적하게 이용 가능
+                풀스크린으로 쾌적하게 이용 가능
             </strong>
             <div style={{ lineHeight: "185%" }}><br></br></div>
 
