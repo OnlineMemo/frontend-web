1462b0de66bada9baaa2cb302d4e40cd3f82b4c5
sahyunjin <tkguswls1106@naver.com>
2025-07-25 23:57:18 +0900

    Chore: 토큰 체크로직 추가 기재

--- src/utils/CheckToken.js
+++ src/utils/CheckToken.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 function CheckToken() {
+    // const isLoggedIn = !!(localStorage.getItem("accessToken") && localStorage.getItem("refreshToken"));
     const storedAccessToken = localStorage.getItem("accessToken");
     const storedRefreshToken = localStorage.getItem("refreshToken");
 
