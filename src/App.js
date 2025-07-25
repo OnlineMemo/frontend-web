d5b0bf77bfefd6e797636246f2a358a193e3993c
sahyunjin <tkguswls1106@naver.com>
2025-07-26 02:12:14 +0900

    Fix: 회원가입 페이지 description 구체화

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function HelmetComponent() {
 
   const getHelmetDescription = () => {
     let helmetDescription = "📝 모든 기기에서 간편하게 메모를 작성하고, 친구와 공동 편집도 가능한 온라인 메모장입니다. 📝";
-    if (pathName === "/signup") helmetDescription = "심플 회원가입 - 생성할 ID/PW만 입력하면, 개인정보 없이 빠르게 가입할 수 있어요.";
+    if (pathName === "/signup") helmetDescription = "심플 회원가입 - 생성할 ID/PW만 입력하고, 개인정보 없이 빠르게 가입해 메모를 관리해요.";  // or '심플 회원가입 - 생성할 ID/PW만 입력하면, 개인정보 없이 빠르게 가입하고 메모를 관리할 수 있어요.'
     else if (pathName === "/notice") helmetDescription = "이용방법 안내 – 웹사이트 주소는 OnlineMemo.kr 이며, 문의는 기재된 메일로 부탁드립니다.";
     else if (pathName === "/download") helmetDescription = "모바일 앱 지원 - 웹은 물론, Android · iOS 앱에서도 쾌적한 풀스크린 환경을 제공합니다.";
     return helmetDescription;
