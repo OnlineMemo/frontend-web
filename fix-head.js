f28237e51598def800f2a80989d698995c273249
sahyunjin <tkguswls1106@naver.com>
2025-10-07 03:54:45 +0900

    [#8] Refactor: hydratable 내 Toast CSS 제거 최적화

--- fix-head.js
+++ fix-head.js
@@ -10,14 +10,16 @@ const uniqueFiles = [...new Set(files)];
 uniqueFiles.forEach(file => {
   let html = fs.readFileSync(file, 'utf8');
 
-  // 첫번째 이외의 모든 중복 Toast CSS 제거
   const toastStyleRegex = /<style[^>]*>[\s\S]*?:root\{--toastify-color-light[\s\S]*?<\/style>/gi;
-  const matches = html.match(toastStyleRegex);
-  if (matches && matches.length > 1) {
-    for (let i = 1; i < matches.length; i++) {
-      html = html.replace(matches[i], '');
-    }
-  }
+  // - 첫번째 이외의 모든 중복 Toast CSS 제거
+  // const matches = html.match(toastStyleRegex);
+  // if (matches && matches.length > 1) {
+  //   for (let i = 1; i < matches.length; i++) {
+  //     html = html.replace(matches[i], '');
+  //   }
+  // }
+  // - 모든 Toast CSS 제거 (런타임에 1회 자동 재생성됨.)
+  html = html.replace(toastStyleRegex, '');
 
   // meta charset을 맨 위로 이동
   html = html.replace(/<meta[^>]*charset[^>]*>/gi, '');
