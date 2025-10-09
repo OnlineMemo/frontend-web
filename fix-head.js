69322a53e40251353fbb09445530d9367e350dcc
sahyunjin <tkguswls1106@naver.com>
2025-10-10 02:18:26 +0900

    [#8] Refactor: Font 및 Lighthouse CLS 수치 최적화

--- fix-head.js
+++ fix-head.js
@@ -4,16 +4,26 @@ const glob = require('glob');
 
 // ================ < Get Files > ================ //
 
+// - html files
 const htmlFiles = [
   ...glob.sync('build/index.html'),
   ...glob.sync('build/**/*.html'),
 ];
 const uniqueHtmlFiles = [...new Set(htmlFiles)];
 
+// - css files
 const cssFiles = [
   ...glob.sync('build/fontawesome/font-awesome.shj.css'),
 ];
 const uniqueCssFiles = [...new Set(cssFiles)];
+
+// - font files
+const fontFiles = [
+  ...glob.sync('build/static/media/fontawesome-webfont.*.woff2'),
+  ...glob.sync('build/static/media/fontawesome-webfont.*.woff'),
+  ...glob.sync('build/static/media/fontawesome-webfont.*.ttf'),
+];
+const uniqueFontFiles = [...new Set(fontFiles)];
 
 
 // ================ < Fix Head > ================ //
@@ -36,7 +46,7 @@ uniqueHtmlFiles.forEach(file => {
   // (비록 런타임에 1회 추가 재생성되지만, SEO 크롤러를 위해 중복 2회까지 허용.)
   const styledComponentsRegex = /<style[^>]*data-styled="active"[^>]*data-styled-version="[^"]*"[^>]*>([\s\S]*?)<\/style>/gi;
   const innerContentSet = new Set();
-  html = html.replace(styledComponentsRegex, (match, innerContent) => {  // innerContent는 <style>여기</style> 내부 CSS 내용을 의미함
+  html = html.replace(styledComponentsRegex, (match, innerContent) => {  // innerContent는 <style>여기</style> 내부 CSS 내용을 의미
     const trimmedContent = innerContent.trim();
     if (!innerContentSet.has(trimmedContent)) {  // 첫번째 내용이면 유지
       innerContentSet.add(trimmedContent);
@@ -51,13 +61,27 @@ uniqueHtmlFiles.forEach(file => {
   html = html.replace(/<meta[^>]*charset[^>]*>/gi, '');
   html = html.replace(/<head[^>]*>/i, match => `${match}<meta charset="UTF-8" />`);
 
-  // - head 태그의 맨 뒤에 <style>fontawesome.css<style> 스타일 삽입
   if (uniqueCssFiles.length > 0) {
+    // - head 태그의 맨 뒤에 <style>fontawesome.css<style> 스타일 교체
     let css = fs.readFileSync(uniqueCssFiles[0], 'utf8');
     css = css.replace(/\/\*[\s\S]*?\*\//g, '');  // CSS 주석 제거 ('/* */' 형태)
-    css = css.replace(/\s+/g, ' ').trim();  // 공백 최소화 (minify 최적화)
+    css = css.replace(/\s+/g, ' ').trim();  // 연속공백 1칸으로 최소화 (minify 최적화)
+
+    // - 삽입한 fontawesome.css 스타일에 빌드된 static 폰트 경로 교체
+    const fontObj = {};
+    for (const fontPath of uniqueFontFiles) {
+      if (!fontObj['woff2'] && fontPath.endsWith('.woff2')) fontObj['woff2'] = fontPath.replace(/^build/, '');
+      if (!fontObj['woff'] && fontPath.endsWith('.woff')) fontObj['woff'] = fontPath.replace(/^build/, '');
+      if (!fontObj['ttf'] && fontPath.endsWith('.ttf')) fontObj['ttf'] = fontPath.replace(/^build/, '');
+      if (fontObj['woff2'] && fontObj['woff'] && fontObj['ttf']) break;
+    }
+    css = css.replace(/url\(['"]?\.\/fontawesome-webfont\.(woff2|woff|ttf)['"]?\)/g, (match, ext) => {  // ext는 확장자를 의미
+      return fontObj[ext] ? `url('${fontObj[ext]}')` : match;
+    });
+
+    // - 최종 스타일 'css & font' 결과물을 'html' 파일에 삽입
     const styleTagWithCss = `<style>${css}</style>`;
-    html = html.replace(/<\/head>/i, `${styleTagWithCss}</head>`);  // 스타일 삽입
+    html = html.replace(/<\/head>/i, `${styleTagWithCss}</head>`);
   }
 
   fs.writeFileSync(file, html, 'utf8');
