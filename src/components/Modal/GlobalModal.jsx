8d76755a014a655e6874faee46dc5e79337dc686
sahyunjin <tkguswls1106@naver.com>
2026-06-29 16:02:48 +0900

    Feat: v2.4.0 임시저장/복구 기능에 대한 공지모달 업데이트

--- src/components/Modal/GlobalModal.jsx
+++ src/components/Modal/GlobalModal.jsx
@@ -23,8 +23,8 @@ function GlobalModal(props) {  // Global Modals Component (with Route)
     const location = useLocation();
 
     const progressStartDateTime = "2025-10-15 20:00";
-    const middleDateTime = "2026-05-02 02:00";
-    const completeEndDateTime = "2026-05-09 00:00";  // or "2025-05-08 23:59"
+    const middleDateTime = "2026-06-29 02:00";
+    const completeEndDateTime = "2026-07-07 00:00";  // or "2026-07-06 23:59"
 
 
     return (
@@ -55,16 +55,16 @@ function GlobalModal(props) {  // Global Modals Component (with Route)
                 modalStyle={{ height: "203.5px", paddingTop: "36.5px", paddingBottom: "47px" }}
                 iconStyle={{ fontSize: "4rem" }} contentStyle={{ lineHeight: "115%" }}
             >
-                [ v 2.3.0 업데이트 완료 ]
+                [ v 2.4.0 업데이트 완료 ]
                 <div style={{ lineHeight: "90%" }}><br></br></div>
-                
-                * 메모 상단고정 기능 추가<br></br>
+
+                * 임시저장 복구 기능 추가<br></br>
                 <div style={{ lineHeight: "5%" }}><br></br></div>
-                * 각 메모 옵션(:)에서 설정<br></br>
+                * 이탈/실패 시 자동 보존<br></br>
                 <div style={{ lineHeight: "5%" }}><br></br></div>
-                * 최대 3개까지 고정 가능
+                * 복구 시 저장 시각 표시<br></br>
                 <div style={{ lineHeight: "5%" }}><br></br></div>
-                * 기기별 독립 설정 지원
+                * 기기별 독립 복구 지원
             </NoticeModal>
         </GlobalWrapper>
     );
